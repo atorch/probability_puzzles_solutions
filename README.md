f6c700858bfc73e058fc0efaa149e3c22280ac66
Adrian Torchiana <adrian.torchiana@gmail.com>
2021-06-26 22:05:42 -0700

    Link in readme and triangle ggplot

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 This repo contains R simulations for checking solutions to (some of) the probability puzzles
 in [my android app](https://play.google.com/store/apps/details?id=atorch.statspuzzles).
 
+For example, [outrageous/puzzle_01.R](outrageous/puzzle_01.R) is a simulation checking the
+solution to [this puzzle](https://github.com/atorch/probability_puzzles/blob/master/app/src/main/res/values/strings.xml#L636-L637).
+
 Are you looking for the android app code?  See https://github.com/atorch/probability_puzzles.
--- getting_serious/puzzle_34.R
+++ getting_serious/puzzle_34.R
@@ -16,8 +16,15 @@ points(0, 0, pch=2)
 points(c(triangle_points[, 1], triangle_points[1, 1]), c(triangle_points[, 2], triangle_points[1, 2]), type="l", col="red")
 
 df <- data.frame(x=circle[, 1], y=circle[, 2])
-p <- ggplot(df, aes(x=x, y=y)) + geom_point() + theme_bw()
+df_triangle <- data.frame(x=c(triangle_points[, 1], triangle_points[1:2, 1]), y=c(triangle_points[, 2], triangle_points[1:2, 2]))
+
+p <- (ggplot(df, aes(x=x, y=y)) +
+      geom_path(size=1.5) +
+      geom_path(size=1.25, color="red", data=df_triangle) +
+      geom_point(aes(x=0.0, y=0.0), colour="blue", shape=3, size=3.5) +
+      theme_void())
 p
+ggsave("circle_and_triangle.png", plot=p, width=4, height=4)
 
 ## Does this particular triangle contain the origin?
 point.in.polygon(0, 0, triangle_points[, 1], triangle_points[, 2])
